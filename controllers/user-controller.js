03071e5f0c1bd74329d3fda498763fdc5078a3ff
martinchiu <martin87713@gmail.com>
2022-02-12 18:35:41 +0800

    refactor: modify add-like route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -123,28 +123,21 @@ const userController = {
       .then(() => res.redirect('back'))
       .catch(err => next(err))
   },
-  addLike: (req, res, next) => {
-    const { restaurantId } = req.params
-    return Promise.all([
-      Restaurant.findByPk(restaurantId),
-      Like.findOne({
+  addLike: async (req, res, next) => {
+    try {
+      const { restaurantId } = req.params
+      const restaurant = await Restaurant.findByPk(restaurantId)
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+      const [like, create] = await Like.findOrCreate({
         where: {
           userId: req.user.id,
           restaurantId
         }
       })
-    ])
-      .then(([restaurant, like]) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        if (like) throw new Error('You have liked this restaurant!')
-
-        return Like.create({
-          userId: req.user.id,
-          restaurantId
-        })
-      })
-      .then(() => res.redirect('back'))
-      .catch(err => next(err))
+      if (!create) throw new Error('You have liked this restaurant!')
+      res.redirect('back')
+    } catch (err) { next(err) }
   },
   removeLike: (req, res, next) => {
     return Like.findOne({
